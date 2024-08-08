ac63f9e16419e176ab79b5488abf2104fddfe4c9
circle08 <hawon9781@naver.com>
2024-08-08 18:39:34 +0900

    Main publishing

---
+++ Qint_iOS/Source/Component/CategoryButton.swift
@@ -0,0 +1,111 @@
+import UIKit
+import SnapKit
+import Then
+
+enum BtType {
+    case fe
+    case be
+    case ios
+    case flutter
+    case custom(title: String, image: String, left: Int, right: Int)
+    
+    var text: String {
+        switch self {
+        case .fe:
+            return "프론트엔드"
+        case .be:
+            return "백엔드"
+        case .ios:
+            return "iOS"
+        case .flutter:
+            return "플러터"
+        case .custom(let title, _, _, _):
+            return title
+        }
+    }
+    var image: String {
+        switch self {
+        case .fe:
+            return "feImage"
+        case .be:
+            return "beImage"
+        case .ios:
+            return "iosImage"
+        case .flutter:
+            return "flutterImage"
+        case .custom(_, let image, _, _):
+            return image
+        }
+    }
+    var left: Int {
+        switch self {
+        case .fe:
+            return 205
+        case .be:
+            return 0
+        case .ios:
+            return 205
+        case .flutter:
+            return 0
+        case .custom(_, _, let left, _):
+            return left
+        }
+    }
+    var right: Int {
+        switch self {
+        case .fe:
+            return 0
+        case .be:
+            return 205
+        case .ios:
+            return 0
+        case .flutter:
+            return 205
+        case .custom(_, _, _, let right):
+            return right
+        }
+    }
+}
+
+class CategoryButton: UIView {
+    let button = UIButton().then {
+        $0.backgroundColor = UIColor(named: "Mint100")
+        $0.layer.cornerRadius = 10
+        $0.adjustsImageWhenHighlighted = false
+    }
+    
+    let label = UILabel().then {
+        $0.textColor = UIColor(named: "Mint300")
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    
+    init(type: BtType) {
+        super.init(frame: .zero)
+        
+        button.setImage(UIImage(named: type.image), for: .normal)
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: CGFloat(type.left), bottom: 0, right: CGFloat(type.right))
+        label.text = type.text
+        
+        add()
+        layout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func add() {
+        self.addSubview(button)
+        button.addSubview(label)
+    }
+    
+    func layout() {
+        button.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(0)
+            $0.left.right.equalToSuperview().inset(0)
+        }
+        label.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
--- Qint_iOS/Source/Scene/Main/MainViewController.swift
+++ Qint_iOS/Source/Scene/Main/MainViewController.swift
@@ -1,5 +1,107 @@
 import UIKit
+import SnapKit
+import Then
 
 class MainViewController: UIViewController {
     
+    let qintLabel = UILabel().then {
+        $0.text = "Q-int"
+        $0.font = UIFont(name: "ZenDots-Regular", size: 24)
+        $0.textColor = UIColor(named: "Gray400")
+    }
+
+    let myButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "person.fill"), for: .normal)
+        $0.tintColor = UIColor(named: "Gray400")
+        $0.imageView?.contentMode = .scaleAspectFill
+        $0.contentHorizontalAlignment = .fill
+        $0.contentVerticalAlignment = .fill
+        $0.addTarget(self, action: #selector(myButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func myButtonTapped() {
+        self.navigationController?.pushViewController(MyViewController(), animated: true)
+    }
+    
+    private let feButton = CategoryButton(type: .fe)
+    private let beButton = CategoryButton(type: .be)
+    private let iosButton = CategoryButton(type: .ios)
+    private let flutterButton = CategoryButton(type: .flutter)
+    
+    let categoryLabel = UILabel().then {
+        $0.text = "카테고리를 한 개 이상 선택해주세요!"
+        $0.textColor = UIColor(named: "Gray400")
+    }
+    
+    let startButton = UIButton().then {
+        $0.QintButton(setTitle: "시작하기", setTitleColor: "White", buttonColor: "Mint300")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.isNavigationBarHidden = true
+        
+        attribute()
+        add()
+        layout()
+    }
+    
+    func attribute() {
+        view.backgroundColor = .white
+    }
+    
+    func add() {
+        [
+            qintLabel,
+            myButton,
+            feButton,
+            beButton,
+            iosButton,
+            flutterButton,
+            categoryLabel,
+            startButton
+        ].forEach{ view.addSubview($0) }
+    }
+    
+    func layout() {
+        qintLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(71)
+            $0.left.equalToSuperview().inset(30)
+        }
+        myButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(71)
+            $0.right.equalToSuperview().inset(30)
+            $0.height.width.equalTo(25)
+        }
+        feButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(150)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(84)
+        }
+        beButton.snp.makeConstraints {
+            $0.top.equalTo(feButton.snp.bottom).offset(16)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(84)
+        }
+        iosButton.snp.makeConstraints {
+            $0.top.equalTo(beButton.snp.bottom).offset(16)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(84)
+        }
+        flutterButton.snp.makeConstraints {
+            $0.top.equalTo(iosButton.snp.bottom).offset(16)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(84)
+        }
+        categoryLabel.snp.makeConstraints {
+            $0.top.equalTo(flutterButton.snp.bottom).offset(24)
+            $0.left.equalToSuperview().inset(24)
+        }
+        startButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(48)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(52)
+        }
+    }
 }
